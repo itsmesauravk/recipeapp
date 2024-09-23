ab0042f69983f60c07caac4b34016ecc948628df
Saurav Karki <sauravkarki10.12@gmail.com>
2024-09-23 19:51:15 +0545

    minor change

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,53 @@
 const express = require('express');
 const app = express();
+
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
+const http = require('http');
+
+
+
+
+
 // Middleware
 app.use(cors({
-    origin: 'https://makehub.vercel.app',
+    // origin: 'http://localhost:3000',
+    origin: ['https://makehub.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH'],
     credentials: true,
-}));
+    })
+);
 
 app.use(express.json());
+
 app.use(cookieParser());
 
-// Routes
-app.use('/api/user', require('./routes/user.routes'));
-app.use('/api/recipe', require('./routes/recipe.routes'));
-app.use('/api/bot', require('./routes/bot.routes'));
 
-// Healthcheck route
-app.get('/api/healthcheck', (req, res) => {
+
+const userRoutes = require('./routes/user.routes');
+const recipeRoutes = require('./routes/recipe.routes');
+const botRoutes = require('./routes/bot.routes');
+
+const authRoutes = require('./auth/authRoutes');
+const { create } = require('./models/user.models');
+
+
+app.use('/',authRoutes)
+app.use('/api', userRoutes);
+app.use('/api/recipe', recipeRoutes);
+app.use('/api', botRoutes);
+
+
+
+
+
+// healthcheck 
+ app.get('/api/healthcheck', (req, res) => {
     res.send('I am alive');
-});
+    });
 
-// Export the app for Vercel serverless function
+
+
 module.exports = app;
